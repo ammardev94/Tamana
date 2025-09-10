df93c1a925d5df6098544fd492b0d08a8e4b89f3
Syed Nazir Hussain <syednazir13@gmail.com>
2025-09-10 15:23:18 +0500

    hotfixes

--- app/Http/Controllers/Admin/PartnerController.php
+++ app/Http/Controllers/Admin/PartnerController.php
@@ -15,7 +15,7 @@ class PartnerController extends Controller
 {
     public function index(): View
     {
-        $partners = Partner::paginate(10);
+        $partners = Partner::orderBy('created_at', 'desc')->paginate(10);
         return view('admin.partners.index', compact('partners'));
     }
 
