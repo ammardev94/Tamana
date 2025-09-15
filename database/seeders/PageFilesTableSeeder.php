a2ff9467ef6343ab435383caac87523ff8a21be1
Syed Nazir Hussain <nazir.h@brande.com>
2025-09-15 09:34:00 +0500

    Page and Meta fixes

--- database/seeders/PageFilesTableSeeder.php
+++ database/seeders/PageFilesTableSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
@@ -13,6 +13,106 @@ class PageFilesTableSeeder extends Seeder
      */
     public function run(): void
     {
-        DB::table('page_files')->insert([]);
+        DB::table('page_files')->insert([
+            [
+                'id' => 1,
+                'name' => 'xSLwnw1y7ISwFuck2hV1oDOP1e0ilsQZW0awM02u.mp4',
+                'path' => 'page-images/1/xSLwnw1y7ISwFuck2hV1oDOP1e0ilsQZW0awM02u.mp4',
+                'ref_id' => 1,
+                'ref_point' => 'section_one_background',
+                'alt_text' => 'Mamo',
+                'created_at' => Carbon::parse('2025-09-08 10:56:26'),
+                'updated_at' => Carbon::parse('2025-09-08 06:52:24'),
+                'deleted_at' => null
+            ],
+            [
+                'id' => 2,
+                'name' => '5ad56pnw74SOSSevOTcuMm6JVeiqgh11kLNOEw8E.jpg',
+                'path' => 'page-images/1/5ad56pnw74SOSSevOTcuMm6JVeiqgh11kLNOEw8E.jpg',
+                'ref_id' => 1,
+                'ref_point' => 'section_four_background',
+                'alt_text' => 'Tanama',
+                'created_at' => Carbon::parse('2025-09-08 10:56:26'),
+                'updated_at' => Carbon::parse('2025-09-09 02:21:50'),
+                'deleted_at' => null
+            ],
+            [
+                'id' => 3,
+                'name' => 'lty934Ocwut0urXv9jLrpg9XKjwWRkw7lTvpFxWr.png',
+                'path' => 'page-images/1/lty934Ocwut0urXv9jLrpg9XKjwWRkw7lTvpFxWr.png',
+                'ref_id' => 1,
+                'ref_point' => 'section_fifth_background',
+                'alt_text' => 'Tanama',
+                'created_at' => Carbon::parse('2025-09-08 10:56:26'),
+                'updated_at' => Carbon::parse('2025-09-09 04:15:40'),
+                'deleted_at' => null
+            ],
+            [
+                'id' => 4,
+                'name' => 'Xf4CRaJ3q4DQ5dcGc8z8qH5dCEWDIJj2zfQzcO1m.mp4',
+                'path' => 'page-images/2/Xf4CRaJ3q4DQ5dcGc8z8qH5dCEWDIJj2zfQzcO1m.mp4',
+                'ref_id' => 2,
+                'ref_point' => 'section_two_background',
+                'alt_text' => 'Tanama',
+                'created_at' => Carbon::parse('2025-09-08 10:56:26'),
+                'updated_at' => Carbon::parse('2025-09-09 04:41:28'),
+                'deleted_at' => null
+            ],
+            [
+                'id' => 5,
+                'name' => 'xbHUkmVXPOJRrzOFEfokMcVjzFWBU5g4WvTaeGUN.png',
+                'path' => 'page-images/2/xbHUkmVXPOJRrzOFEfokMcVjzFWBU5g4WvTaeGUN.png',
+                'ref_id' => 2,
+                'ref_point' => 'section_three_background',
+                'alt_text' => 'Tanama',
+                'created_at' => Carbon::parse('2025-09-08 10:56:26'),
+                'updated_at' => Carbon::parse('2025-09-09 04:55:28'),
+                'deleted_at' => null
+            ],
+            [
+                'id' => 6,
+                'name' => 'xbHUkmVXPOJRrzOFEfokMcVjzFWBU5g4WvTaeGUN.png',
+                'path' => 'page-images/2/xbHUkmVXPOJRrzOFEfokMcVjzFWBU5g4WvTaeGUN.png',
+                'ref_id' => 2,
+                'ref_point' => 'section_fourth_img',
+                'alt_text' => 'Tanama',
+                'created_at' => Carbon::parse('2025-09-08 10:56:26'),
+                'updated_at' => Carbon::parse('2025-09-09 04:55:28'),
+                'deleted_at' => null
+            ],
+            [
+                'id' => 7,
+                'name' => 'xbHUkmVXPOJRrzOFEfokMcVjzFWBU5g4WvTaeGUN.png',
+                'path' => 'page-images/2/xbHUkmVXPOJRrzOFEfokMcVjzFWBU5g4WvTaeGUN.png',
+                'ref_id' => 2,
+                'ref_point' => 'section_seventh_img',
+                'alt_text' => 'Tanama',
+                'created_at' => Carbon::parse('2025-09-08 10:56:26'),
+                'updated_at' => Carbon::parse('2025-09-09 04:55:28'),
+                'deleted_at' => null
+            ],
+            [
+                'id' => 8,
+                'name' => 'xbHUkmVXPOJRrzOFEfokMcVjzFWBU5g4WvTaeGUN.png',
+                'path' => 'page-images/2/xbHUkmVXPOJRrzOFEfokMcVjzFWBU5g4WvTaeGUN.png',
+                'ref_id' => 4,
+                'ref_point' => 'section_two_img',
+                'alt_text' => 'Tanama',
+                'created_at' => Carbon::parse('2025-09-08 10:56:26'),
+                'updated_at' => Carbon::parse('2025-09-09 04:55:28'),
+                'deleted_at' => null
+            ],
+            [
+                'id' => 9,
+                'name' => 'xbHUkmVXPOJRrzOFEfokMcVjzFWBU5g4WvTaeGUN.png',
+                'path' => 'page-images/2/xbHUkmVXPOJRrzOFEfokMcVjzFWBU5g4WvTaeGUN.png',
+                'ref_id' => 5,
+                'ref_point' => 'section_two_img',
+                'alt_text' => 'Tanama',
+                'created_at' => Carbon::parse('2025-09-08 10:56:26'),
+                'updated_at' => Carbon::parse('2025-09-09 04:55:28'),
+                'deleted_at' => null
+            ]
+        ]);
     }
 }
--- database/seeders/PagesTableSeeder.php
+++ database/seeders/PagesTableSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
@@ -13,6 +13,103 @@ class PagesTableSeeder extends Seeder
      */
     public function run(): void
     {
-        DB::table('pages')->insert([]);
+        DB::table('pages')->insert([
+            [
+                'id' => 1,
+                'type' => '1',
+                'title' => 'Home',
+                'slug' => 'home',
+                'page_title' => 'Omnis nisi reiciendi',
+                'page_description' => 'Quis ad sed dolores .',
+                'visibility' => 'no-follow',
+                'status' => '1',
+                'canonical_url' => 'https://www.xikuwyniw.cm',
+                'has_meta' => null,
+                'added_by' => 1,
+                'created_at' => Carbon::parse('2025-09-08 05:38:38'),
+                'updated_at' => Carbon::parse('2025-09-08 06:58:26'),
+                'deleted_at' => null,
+            ],
+            [
+                'id' => 2,
+                'type' => '1',
+                'title' => 'About Us',
+                'slug' => 'about-us',
+                'page_title' => 'Amet nulla in expli',
+                'page_description' => 'Magni aliquam dolor .',
+                'visibility' => 'no-follow',
+                'status' => '0',
+                'canonical_url' => 'https://www.vekozolyredore.ca',
+                'has_meta' => null,
+                'added_by' => 1,
+                'created_at' => Carbon::parse('2025-09-09 04:26:53'),
+                'updated_at' => Carbon::parse('2025-09-09 04:27:12'),
+                'deleted_at' => null,
+            ],
+            [
+                'id' => 3,
+                'type' => '1',
+                'title' => 'News Main',
+                'slug' => 'news-main',
+                'page_title' => 'Iusto dolor eveniet',
+                'page_description' => 'Quas ut natus sapien.',
+                'visibility' => 'no-follow',
+                'status' => '1',
+                'canonical_url' => 'https://www.cykakapaby.ws',
+                'has_meta' => null,
+                'added_by' => 1,
+                'created_at' => Carbon::parse('2025-09-09 06:08:00'),
+                'updated_at' => Carbon::parse('2025-09-09 06:08:00'),
+                'deleted_at' => null,
+            ],
+            [
+                'id' => 4,
+                'type' => '1',
+                'title' => 'Career',
+                'slug' => 'career',
+                'page_title' => 'Et ipsum sit dolor',
+                'page_description' => 'Aut nostrum anim dol.',
+                'visibility' => 'no-index',
+                'status' => '1',
+                'canonical_url' => 'https://www.fomowakodup.me',
+                'has_meta' => null,
+                'added_by' => 1,
+                'created_at' => Carbon::parse('2025-09-11 23:53:18'),
+                'updated_at' => Carbon::parse('2025-09-11 23:53:32'),
+                'deleted_at' => null,
+            ],
+            [
+                'id' => 5,
+                'type' => '1',
+                'title' => 'Contact Us',
+                'slug' => 'contact-us',
+                'page_title' => 'Deserunt in aut in d',
+                'page_description' => 'Amet, tempor atque q.',
+                'visibility' => 'no-follow',
+                'status' => '1',
+                'canonical_url' => 'https://www.gatu.org.uk',
+                'has_meta' => null,
+                'added_by' => 1,
+                'created_at' => Carbon::parse('2025-09-12 00:16:27'),
+                'updated_at' => Carbon::parse('2025-09-12 00:16:42'),
+                'deleted_at' => null,
+            ],
+            [
+                'id' => 6,
+                'type' => '1',
+                'title' => 'ESG',
+                'slug' => 'esg',
+                'page_title' => 'Dolore earum officia',
+                'page_description' => 'Neque culpa molestia.',
+                'visibility' => 'no-index',
+                'status' => '1',
+                'canonical_url' => 'https://www.myxolazofu.ca',
+                'has_meta' => null,
+                'added_by' => 1,
+                'created_at' => Carbon::parse('2025-09-12 04:10:24'),
+                'updated_at' => Carbon::parse('2025-09-12 04:10:24'),
+                'deleted_at' => null,
+            ]
+        ]);
     }
 }
