cb2181353d6c9a8b246e563b515754cee0d88c04
Syed Nazir Hussain <nazir.h@brande.com>
2025-09-16 10:21:19 +0500

    hotfixes

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -2,15 +2,30 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
+use App\Models\News;
+use App\Models\Team;
+use App\Models\Service;
+use App\Models\Portfolio;
 use Illuminate\View\View;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class DashboardController extends Controller
 {
     public function index(Request $request): View
     {
-        return view('admin.index');
+        $news = News::count();
+        $portfolios = Portfolio::count();
+        $teams = Team::count();
+        $services = Service::count();
+
+        $dashboardCounts = [
+            "news"          => $news,
+            "portfolios"    => $portfolios,
+            "teams"         => $teams,
+            "services"      => $services
+        ];
+
+        return view('admin.index', $dashboardCounts);
     }
 }
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -21,6 +21,87 @@
 <!-- /Page Header -->
 
 <div class="row">
+
+    <!-- Total Students -->
+    <div class="col-xxl-3 col-sm-6 d-flex">
+        <a href="{{ route('admin.news.index') }}" class="card flex-fill animate-card border-0">
+            <div class="card-body">
+                <div class="d-flex align-items-center">
+                    <div class="avatar avatar-xl bg-danger-transparent me-2 p-1">
+                        <img src="{{ asset('assets/img/icons/company-icon-04.svg') }}" alt="img">
+                    </div>
+                    <div class="overflow-hidden flex-fill">
+                        <div class="d-flex align-items-center justify-content-between">
+                            <h2 class="counter">{{ $news }}</h2>
+                        </div>
+                        <p>Total News</p>
+                    </div>
+                </div>
+            </div>
+        </a>
+    </div>
+    <!-- /Total Students -->
+
+    <!-- Total Teachers -->
+    <div class="col-xxl-3 col-sm-6 d-flex">
+        <a href="{{ route('admin.portfolio.index') }}" class="card flex-fill animate-card border-0">
+            <div class="card-body">
+                <div class="d-flex align-items-center">
+                    <div class="avatar avatar-xl me-2 bg-secondary-transparent p-1">
+                        <img src="{{ asset('assets/img/icons/company-icon-04.svg') }}" alt="img">
+                    </div>
+                    <div class="overflow-hidden flex-fill">
+                        <div class="d-flex align-items-center justify-content-between">
+                            <h2 class="counter">{{ $portfolios }}</h2>
+                        </div>
+                        <p>Total Portfolio</p>
+                    </div>
+                </div>
+            </div>
+        </a>
+    </div>
+    <!-- /Total Teachers -->
+
+    <!-- Total Staff -->
+    <div class="col-xxl-3 col-sm-6 d-flex">
+        <a href="{{ route('admin.teams.index') }}" class="card flex-fill animate-card border-0">
+            <div class="card-body">
+                <div class="d-flex align-items-center">
+                    <div class="avatar avatar-xl me-2 bg-warning-transparent p-1">
+                        <img src="{{ asset('assets/img/icons/company-icon-04.svg') }}" alt="img">
+                    </div>
+                    <div class="overflow-hidden flex-fill">
+                        <div class="d-flex align-items-center justify-content-between">
+                            <h2 class="counter">{{ $teams }}</h2>
+                        </div>
+                        <p>Total Teams</p>
+                    </div>
+                </div>
+            </div>
+        </a>
+    </div>
+    <!-- /Total Staff -->
+
+    <!-- Total Subjects -->
+    <div class="col-xxl-3 col-sm-6 d-flex">
+        <a href="{{ route('admin.services.index') }}" class="card flex-fill animate-card border-0">
+            <div class="card-body">
+                <div class="d-flex align-items-center">
+                    <div class="avatar avatar-xl me-2 bg-success-transparent p-1">
+                        <img src="{{ asset('assets/img/icons/company-icon-04.svg') }}" alt="img">
+                    </div>
+                    <div class="overflow-hidden flex-fill">
+                        <div class="d-flex align-items-center justify-content-between">
+                            <h2 class="counter">{{ $services }}</h2>
+                        </div>
+                        <p>Total Services</p>
+                    </div>
+                </div>
+            </div>
+        </a>
+    </div>
+    <!-- /Total Subjects -->
+
 </div>
 
 @endsection
